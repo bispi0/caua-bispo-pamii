392a2e4eef5d18d4b7512dd5f3ac93dcd8231d09
bispi0 <140127228+bispi0@users.noreply.github.com>
2026-06-22 21:14:31 -0300

    Refactor RootLayout to include SafeAreaProvider
    Wrap the Stack component in a SafeAreaProvider and add a StatusBar.

--- my-app/app/_layout.tsx
+++ my-app/app/_layout.tsx
@@ -1,5 +1,22 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      {/* Define a cor da barra de status do celular (bateria, hora, etc.) como escura */}
+      <StatusBar style="dark" />
+      
+      {/* Configura a navegação das telas */}
+      <Stack>
+        <Stack.Screen 
+          name="index" 
+          options={{ 
+            headerShown: false // Esconde a barra predefinida do Expo para usarmos o nosso cabeçalho bonito
+          }} 
+        />
+      </Stack>
+    </SafeAreaProvider>
+  );
 }
